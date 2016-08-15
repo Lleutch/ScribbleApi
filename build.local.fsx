ac117a20ed453d3fd6f372747895b45917103378
Fahd <fa1915@ic.ac.uk>
2016-08-15 06:45:14 +0200

    Modify .deployment build.local.fsx and web.config

--- build.local.fsx
+++ build.local.fsx
@@ -1,27 +1,23 @@
 #r "packages/Suave/lib/net40/Suave.dll"
 #r "packages/FAKE/tools/FakeLib.dll"
 #load "api.fsx"
-
 open Api
+open Fake
 open System
 open System.IO
-open Fake
+open Suave
+open Suave.Http
+open Suave.Web
 
-Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let serverConfig =
+    let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
+    { defaultConfig with
+        homeFolder = Some __SOURCE_DIRECTORY__
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Warn
+        bindings = [ HttpBinding.mk HTTP System.Net.IPAddress.Loopback port ] }
 
-// Step 2. Use the packages
+Target "run" (fun _ ->
+    startWebServer serverConfig app
+)
 
-#r "packages/Suave/lib/net40/Suave.dll"
-
-open Suave // always open suave
-open Suave.Successful // for OK-result
-open Suave.Web // for config
-open System.Net
-
-let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
-
-let serverConfig = 
-    {defaultConfig with
-        bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
-    } 
-startWebServer serverConfig app
+RunTargetOrDefault "run"
